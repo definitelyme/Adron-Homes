e88823687c95ec540aa770b2c8083478408ba72e
Brendan Ejike <ejike.br@gmail.com>
2020-01-07 22:26:18 +0100

    First commit

---
+++ app/src/main/java/dev/onekode/adronhomes/MainActivity.java
@@ -0,0 +1,64 @@
+package dev.onekode.adronhomes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.google.android.material.button.MaterialButton;
+import com.makeramen.roundedimageview.RoundedImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private RoundedImageView userImageView;
+    private ConstraintLayout buyButton;
+    private ConstraintLayout sellButton;
+    private ConstraintLayout rentButton;
+    private AutoCompleteTextView locationSpinner;
+    private MaterialButton creatorButton, filterButton;
+    private View selectedButton;
+
+    @Override
+    @com.google.android.material.imageview.ExperimentalImageView
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        );
+
+        userImageView = findViewById(R.id.main_activity_user_imageView);
+        buyButton = findViewById(R.id.main_activity_buy_button);
+        sellButton = findViewById(R.id.main_activity_sell_button);
+        rentButton = findViewById(R.id.main_activity_rent_button);
+        locationSpinner = findViewById(R.id.main_activity_location_spinner);
+        creatorButton = findViewById(R.id.main_activity_creator_btn);
+        filterButton = findViewById(R.id.main_activity_filter_btn);
+        
+        userImageView.bringToFront();
+
+        buyButton.setOnClickListener(this::toggleButton);
+        sellButton.setOnClickListener(this::toggleButton);
+        rentButton.setOnClickListener(this::toggleButton);
+        creatorButton.setOnClickListener(v -> {
+            startActivity(new Intent(this, SearchActivity.class));
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        });
+    }
+
+    private void toggleButton(View aView) {
+        if (selectedButton != null)
+            selectedButton.setSelected(false);
+        // Note: I created a drawable selector -> main_activity_btn_selector
+        // This drawable is incharge of toggling the selected and unselected drawables
+        // If you still don't get it, ask & i'll explain better
+        aView.setSelected(!aView.isSelected());
+        selectedButton = aView;
+    }
+}
